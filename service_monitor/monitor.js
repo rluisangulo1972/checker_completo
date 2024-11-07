990073a95299803309d5e0bba73b9af8dc2b0585
rluisangulo1972 <rluisangulo1972@gmail.com>
2024-11-06 19:01:56 -0500

    rama donde se crea las imagenes de los servicios para dockerhub

--- service_monitor/monitor.js
+++ service_monitor/monitor.js
@@ -11,9 +11,15 @@ const io = require('socket.io')(4000, {
     }
 });
 const cors = require('cors');
+// inicializamos las variables de entorno para que sea ajustable a lo deseado
+// estas variables estarán contempladas en el archivo docker-compose.yml
 const REDIS_HOST = process.env.REDIS_HOST;
+const REDIS_PORT = process.env.REDIS_PORT;
+const BACKEND_INICIAL_NAME = process.env.BACKEND_INICIAL_NAME;
 const BACKEND_INICIAL_HOST = process.env.BACKEND_INICIAL_HOST;
-const clientRedis = redis.createClient({url: `redis://${REDIS_HOST}:6379`});
+const BACKEND_INICIAL_PORT = process.env.BACKEND_INICIAL_PORT;
+const clientRedis = redis.createClient({url: `redis://${REDIS_HOST}:${REDIS_PORT}`});
+// ---------------------------------------------------------------------------------
 const axios = require('axios');
 clientRedis.connect();
 
@@ -24,7 +30,7 @@ io.on('connection', (socket) => {
 // creando al función que realizará el servicio de monitor al backend_inicial
 const checkBackendInicial = async () => {
     try {
-        await axios.get(`http://${BACKEND_INICIAL_HOST}:3000`);
+        await axios.get(`http://${BACKEND_INICIAL_HOST}:${BACKEND_INICIAL_PORT}`);
         clientRedis.set('service_status', 'saludable')
     } catch (err) {
         clientRedis.set('service_status', 'no_saludable')
@@ -36,7 +42,7 @@ const checkBackendInicial = async () => {
 // leyenda --> 5000 = 5 segundos ; 10000 = 10 segundos
 setInterval(async () => {
     const status = await clientRedis.get('service_status');
-    io.emit('status_updated', {service: 'backend_inicial', status});
+    io.emit('status_updated', {service: BACKEND_INICIAL_NAME, status});
     console.log(`Evento emitido: ${status}`)
 }, 5000)
 
